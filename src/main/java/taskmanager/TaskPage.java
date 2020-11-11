dd0445cbf7057465d8e40da5f9c3f96e2bd266a7
eflodev <56899167+eflodev@users.noreply.github.com>
2020-11-10 22:05:46 -0600

    Update TaskPage.java

--- src/main/java/taskmanager/TaskPage.java
+++ src/main/java/taskmanager/TaskPage.java
@@ -1639,7 +1639,7 @@ public class TaskPage extends javax.swing.JFrame {
         // Delete the task category with @category_name, whose team_ID is the ID of the current team, from the database.
         boolean page_loaded = true;
         DBConnection.connect();
-        PreparedStatement ps = DBConnection.prepared_statement("UPDATE TASKCATEGORIES SET DELETED = 'Y' "+
+        PreparedStatement ps = DBConnection.prepared_statement("DELETE FROM TASKCATEGORIES "+
                 "WHERE NAME = ? AND TEAM_ID = ?");
         page_loaded = (ps != null) ? DBConnection.set_statement_value(ps, 1, category_name) : false;
         page_loaded = page_loaded ? DBConnection.set_statement_value(ps, 2, SystemController.current_team.team_ID()) : false;
